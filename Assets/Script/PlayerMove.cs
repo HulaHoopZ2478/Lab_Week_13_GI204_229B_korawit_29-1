2bbdb59b2385373f9b71cfb7b4a6df8f0f02b286
HulaHoopZ2478 <korawit.nakt@bumail.net>
2026-04-23 23:24:59 +0700

    add jump

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,11 +4,14 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rb2d;
     public float speed;
+    public float JumpForce;
+    public bool isJumping;
+
     float move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       rb2d = GetComponent<Rigidbody2D>(); 
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,6 +20,26 @@ public class PlayerMove : MonoBehaviour
         move = Input.GetAxis("Horizontal");
 
         rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
-		Debug.Log(move);
-	}
+        Debug.Log(move);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
+            Debug.Log("Jump!");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
 }
